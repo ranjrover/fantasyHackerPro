f0a43f399100ddc36c8830fc54c1b19044ef5139
TrevorJFerguson <111551152+TrevorJFerguson@users.noreply.github.com>
2022-11-11 19:55:59 -0800

    added flashing 'loading ranks' heading while waiting

--- fantasypros.js
+++ fantasypros.js
@@ -7,20 +7,40 @@ setTimeout(() => {
     chrome.runtime.sendMessage({ code: 'ranks-filled', ranks: proRanks }, function() {
         console.log('done');
     });
-}, 9999);
+}, 5000);
 
-// see if the script is running
-let heading = document.querySelector("#rankings-app > div.inner > div.ranking-header-wrap--padding-one-banner > div.ranking-header-wrap.clearfix.hide-print > div > div.rankings-page__heading-wrap > h1");
-heading.style.color = 'RGB(4, 199, 79)';
 let headings = ['LOADING RANKS', 'LOADING RANKS.', 'LOADING RANKS..', 'LOADING RANKS...'];
+let colors = ['red', 'gray', 'red', 'gray'];
+const runHeading = flashHeading(headings, colors);
+limitedInterval(() => runHeading(), 500, 6000);
 
+function flashHeading(array, array2) {
+    let heading = document.querySelector("#rankings-app > div.inner > div.ranking-header-wrap--padding-one-banner > div.ranking-header-wrap.clearfix.hide-print > div > div.rankings-page__heading-wrap > h1");
+    let count = 0;
+    return function() {
+        if (count > array.length - 1) {
+            count = 0;
+        } 
+        heading.style.color = array2[count]
+        heading.innerText = array[count];
+        heading.style.fontsize = '24px';
+        count++;
+        return [count, 'inner function has run'];
+    }
+}
 
-
+function limitedInterval(callback, wait, limit) {
+    let totalTime = wait;
+    while (totalTime <= limit) {
+        setTimeout(() => {
+            callback();
+        }, totalTime)
+        totalTime += wait;
+    }
+}
 
 function fillRanks() {
     const rankValues = {};
-    // get our parentDOM ie the table of players and ranks
-    // const tableDOM = document.getElementById("ranking-table");
     const allPlayers = document.querySelectorAll('.player-cell.player-cell__td');
 
     for (let i = 0; i < 500; i++) {
@@ -36,3 +56,4 @@ function fillRanks() {
     
     return rankValues;
 }
+
